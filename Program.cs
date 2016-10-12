e4be19f6cbda9a48154593e237506b05ad4da7d3
roadman1991 <schwarzer_florian@web.de>
2016-10-12 12:36:53 +0200

    reter
    erter

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wer_Reader wer = new Wer_Reader();
+            wer.WerPath = @"C:\Users\f.schwarzer\Desktop\SteinamProjekt-master\SteinamProjekt\Guenther\AppCrash_AD2F1837.HPPrint_5f3be62d972dbae282e3eb41d381fa33e129b39f_9a4a8176_18f7b9a3\Report.wer";
+            wer.ReadWer(wer.WerPath);
+            wer.AppPath = wer.ReadKeys("AppPath");
+            wer.AppName = wer.ReadKeys("AppName");
+            wer.ReportType = int.Parse(wer.ReadKeys("ReportType"));
+            wer.UserName = wer.ReadUserName(wer.WerPath);
+            wer.Mac = wer.ReadMac();
+            wer.FileDate = wer.GetFileCreationDate(wer.WerPath);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Wer-Reader.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApplication1
+{
+    class Wer_Reader
+    {
+        private DateTime _fileDate;
+        public DateTime FileDate
+        {
+            get { return _fileDate; }
+            set { _fileDate = value; }
+        }
+
+        private int _reportType;
+        public int ReportType
+        {
+            get { return _reportType; }
+            set { _reportType = value; }
+        }
+
+        private string _appName;
+        public string AppName
+        {
+            get { return _appName; }
+            set { _appName = value; }
+        }
+
+        private string _appPath;
+        public string AppPath
+        {
+            get { return _appPath; }
+            set { _appPath = value; }
+        }
+
+        private string _mac;
+        public string Mac
+        {
+            get { return _mac; }
+            set { _mac = value; }
+        }
+
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+
+        private int _reportId;
+        public int ReportId
+        {
+            get { return _reportId; }
+            set { _reportId = value; }
+        }
+
+        private string _werPath;
+        public string WerPath
+        {
+            get { return _werPath; }
+            set { _werPath = value; }
+        }
+
+        Dictionary<string, string> _werFileContent =
+        new Dictionary<string, string>();
+
+        public void WerReader()
+        {
+        }
+        public string ReadUserName(string werPath)
+        {
+            string _newPath = Path.GetFullPath(Path.Combine(werPath, @"..\..\"));
+            return Path.GetFileName(Path.GetDirectoryName(_newPath));
+        }
+
+        public string ReadMac()
+        {
+            string macAddr =
+            (
+                from nic in NetworkInterface.GetAllNetworkInterfaces()
+                where nic.OperationalStatus == OperationalStatus.Up
+                select nic.GetPhysicalAddress().ToString()
+            ).FirstOrDefault();
+
+            return macAddr;
+        }
+
+        public void ReadWer(string Path)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path))
+                {
+                    string _line;
+                    while ((_line = sr.ReadLine()) != null)
+                    {
+                        _line = _line.Replace("\0", "");
+                        string[] keyvalue = _line.Split('=');
+                        if (keyvalue.Length == 2)
+                        {
+                            _werFileContent.Add(keyvalue[0], keyvalue[1]);
+                        }
+                    }
+                }
+                _werFileContent.ToList().ForEach(x => Console.WriteLine(x.Key + "=" + x.Value));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        public string ReadKeys(string key) 
+        {
+            return _werFileContent[key];
+        }
+
+        public DateTime GetFileCreationDate(string WerFilePath) 
+        {
+            return File.GetCreationTime(WerFilePath);
+        }
+    }
+}
